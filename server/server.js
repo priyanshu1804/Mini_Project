5bf726050a3a2a5c55af8829820347a0e6e1ba74
priyanshu1520 <priyanshu.thakur024@gmail.com>
2024-12-01 17:22:21 +0530

    fourth commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express=require('express');
 const app=express();
-app.use('/',(req,res)=>{
-    res.send('Hello World');
-});
-app.listen(3000,()=>{
+const User = require('./models/User');
+const Song=require('./models/Song');
+const authRoutes=require('./routes/auth');
+const songRoutes=require('./routes/song');
+const playlistRoutes=require('./routes/playlist');
+app.use(express.json());
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+app.use('/auth',authRoutes);
+app.use('/song',songRoutes);
+app.use('/playlist',playlistRoutes);
+app.listen(PORT,()=>{
     console.log('Server is running on port 3000');
 });
